3ee579f69a73691a403d2d1cda7cd32a3604dfc7
Daniel Lipari <daniel.lipari@georgebrown.ca>
2025-01-17 17:30:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const inputFile = 'input_countries.csv';
+const canadaFile = 'canada.txt';
+const usaFile = 'usa.txt';
+
+// Delete existing files if they exist
+[canadaFile, usaFile].forEach((file) => {
+  if (fs.existsSync(file)) {
+    fs.unlinkSync(file);
+    console.log(`${file} deleted`);
+  }
+});
+
+// Read the CSV and process data
+fs.createReadStream(inputFile)
+  .pipe(csv())
+  .on('data', (row) => {
+    const { country } = row;
+
+    if (country.toLowerCase() === 'canada') {
+      fs.appendFileSync(canadaFile, `${Object.values(row).join(',')}\n`);
+    } else if (country.toLowerCase() === 'united states') {
+      fs.appendFileSync(usaFile, `${Object.values(row).join(',')}\n`);
+    }
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed.');
+    console.log('Filtered data written to canada.txt and usa.txt.');
+  });
